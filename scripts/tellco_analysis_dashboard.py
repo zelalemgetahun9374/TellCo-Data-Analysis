9bece1a990eacd8d00e87f60388ccd8499fcf658
zelalemgetahun9374 <zelalemgetahun9374@gmail.com>
2021-07-17 22:49:05 +0300

    fix: fixed dashboard

--- scripts/tellco_analysis_dashboard.py
+++ scripts/tellco_analysis_dashboard.py
@@ -1,14 +1,14 @@
 import numpy as np
 import pandas as pd
-import pandas_profiling
+# import pandas_profiling
 import streamlit as st
-import altair as alt
-import matplotlib.pyplot as plt
-import plotly.express as px
-import plotly.figure_factory as ff
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
-from streamlit_pandas_profiling import st_profile_report
+# import altair as alt
+# import matplotlib.pyplot as plt
+# import plotly.express as px
+# import plotly.figure_factory as ff
+# from plotly.subplots import make_subplots
+# import plotly.graph_objects as go
+# from streamlit_pandas_profiling import st_profile_report
 
 st.set_page_config(page_title="Tellco Data", layout="wide")
 
@@ -38,59 +38,59 @@ def displayData(df):
     st.write(df)
 
 
-def selectHashTag(df):
-    hashTags = st.multiselect("choose combaniation of hashtags", list(df['hashtags'].unique()))
-    if hashTags:
-        df = df[np.isin(df, hashTags).any(axis=1)]
-        st.write(df)
+# def selectHashTag(df):
+#     hashTags = st.multiselect("choose combaniation of hashtags", list(df['hashtags'].unique()))
+#     if hashTags:
+#         df = df[np.isin(df, hashTags).any(axis=1)]
+#         st.write(df)
 
 
-def advanced_exploration(df, suppress_st_warning=True):
-    df = df.drop(columns=["Bearer Id"])
-    pr = df.profile_report(explorative=True)
-    st_profile_report(pr)
+# def advanced_exploration(df, suppress_st_warning=True):
+#     df = df.drop(columns=["Bearer Id"])
+#     pr = df.profile_report(explorative=True)
+#     st_profile_report(pr)
 
 
-def plotly_bar_sentiment_friends(df):
-    st.markdown("## 1. Sentiment vs Friends count")
-    st.write("The following bar chart shows the number of friends based on the sentiment of each tweet.")
-    fig = px.bar(df, x='sentiment', y='friends_count', color="possibly_sensitive", barmode='group', width=1000)
-    st.plotly_chart(fig)
+# def plotly_bar_sentiment_friends(df):
+#     st.markdown("## 1. Sentiment vs Friends count")
+#     st.write("The following bar chart shows the number of friends based on the sentiment of each tweet.")
+#     fig = px.bar(df, x='sentiment', y='friends_count', color="possibly_sensitive", barmode='group', width=1000)
+#     st.plotly_chart(fig)
 
-def plotly_bar_original_author_retweet(df):
-    count = list(df["original_author"].value_counts().head(10).index)
-    df = df[np.isin(df, count).any(axis=1)]
-    st.markdown("## 3. Original authors vs Retweet count")
-    st.write("The following bar chart shows the number of retweets for the top 10 original authors. Here we can understand that even if PuneUpdater is the highest original author of all, he has very few retweets.")
-    fig = px.bar(df, x='original_author', y='retweet_count', color="sentiment", barmode='group', width=1000)
-    st.plotly_chart(fig)
+# def plotly_bar_original_author_retweet(df):
+#     count = list(df["original_author"].value_counts().head(10).index)
+#     df = df[np.isin(df, count).any(axis=1)]
+#     st.markdown("## 3. Original authors vs Retweet count")
+#     st.write("The following bar chart shows the number of retweets for the top 10 original authors. Here we can understand that even if PuneUpdater is the highest original author of all, he has very few retweets.")
+#     fig = px.bar(df, x='original_author', y='retweet_count', color="sentiment", barmode='group', width=1000)
+#     st.plotly_chart(fig)
 
-def plotly_bar_source_retweet(df):
-    count = list(df["source"].value_counts().head(5).index)
-    df = df[np.isin(df, count).any(axis=1)]
-    st.markdown("## 4. Source vs Retweet count")
-    st.write("The following bar chart shows the number of retweets for the top 5 sources.")
-    fig = px.bar(df, x='source', y='retweet_count', color="sentiment", barmode='group', width=1000)
-    st.plotly_chart(fig)
+# def plotly_bar_source_retweet(df):
+#     count = list(df["source"].value_counts().head(5).index)
+#     df = df[np.isin(df, count).any(axis=1)]
+#     st.markdown("## 4. Source vs Retweet count")
+#     st.write("The following bar chart shows the number of retweets for the top 5 sources.")
+#     fig = px.bar(df, x='source', y='retweet_count', color="sentiment", barmode='group', width=1000)
+#     st.plotly_chart(fig)
 
-def plotly_facet(df):
-    source = list(df["source"].value_counts().head(3).index)
-    df = df[np.isin(df, source).any(axis=1)]
-    fig = px.bar(df, x="sentiment", y="friends_count",
-             facet_row="possibly_sensitive", facet_col="source", width=1000, height=600)
-    st.markdown("## 5. Sentiment vs Retweet count vs Source vs Possibly sensitive")
-    st.write("The following faceted subplots show the number of friends based on sentiments and sensetiveness for the top 3 sources grouped .")
-    st.plotly_chart(fig)
+# def plotly_facet(df):
+#     source = list(df["source"].value_counts().head(3).index)
+#     df = df[np.isin(df, source).any(axis=1)]
+#     fig = px.bar(df, x="sentiment", y="friends_count",
+#              facet_row="possibly_sensitive", facet_col="source", width=1000, height=600)
+#     st.markdown("## 5. Sentiment vs Retweet count vs Source vs Possibly sensitive")
+#     st.write("The following faceted subplots show the number of friends based on sentiments and sensetiveness for the top 3 sources grouped .")
+#     st.plotly_chart(fig)
 
-def authorPie(df):
-    dflocationCount = pd.DataFrame({'Tweet_count': df.groupby(['original_author'])['clean_text'].count()}).reset_index()
-    dflocationCount = dflocationCount.sort_values("Tweet_count", ascending=False)
-    dflocationCount.loc[dflocationCount['Tweet_count'] < 5, 'original_author'] = 'Other authors'
-    fig = px.pie(dflocationCount, values='Tweet_count', names='original_author', width=800, height=500)
-    fig.update_traces(textposition='inside', textinfo='percent+label')
-    st.markdown("## 2. Original authors")
-    st.write("The following pie chart shows top original authors based on their count of tweets. Note that authors with less than 5 tweets are grouped as other authors.")
-    st.plotly_chart(fig)
+# def authorPie(df):
+#     dflocationCount = pd.DataFrame({'Tweet_count': df.groupby(['original_author'])['clean_text'].count()}).reset_index()
+#     dflocationCount = dflocationCount.sort_values("Tweet_count", ascending=False)
+#     dflocationCount.loc[dflocationCount['Tweet_count'] < 5, 'original_author'] = 'Other authors'
+#     fig = px.pie(dflocationCount, values='Tweet_count', names='original_author', width=800, height=500)
+#     fig.update_traces(textposition='inside', textinfo='percent+label')
+#     st.markdown("## 2. Original authors")
+#     st.write("The following pie chart shows top original authors based on their count of tweets. Note that authors with less than 5 tweets are grouped as other authors.")
+#     st.plotly_chart(fig)
 
 # because the data in the database doesn't change, we need to call loadData() only once
 df = loadData()
@@ -111,6 +111,6 @@ if page == "Data table":
 #     plotly_bar_original_author_retweet(df)
 #     plotly_bar_source_retweet(df)
 #     plotly_facet(df)
-else:
-    advanced_exploration(df)
+# else:
+#     advanced_exploration(df)
 st.write('Tellco Data Analysis')
